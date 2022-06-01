e9b0599d9369c7ad1073541e799d33fab6dbbedf
jonbitgood <jon@dbs.org>
2022-06-01 17:01:01 +0000

    Deploying to package from @ digitalbiblesociety/sveltekit-datatables@dd153bb46a22f7e0efb91549620c74b204a23fd5 🚀

--- index.js
+++
@@ -1,9 +0,0 @@
-import Datatable from './Datatable.svelte'
-import SearchInput from './components/SearchInput.svelte'
-import ColumnFilterInputs from './components/ColumnFilterInputs.svelte'
-
-export {
-    Datatable,
-    SearchInput,
-    ColumnFilterInputs
-}
