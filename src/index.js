dd153bb46a22f7e0efb91549620c74b204a23fd5
Jon <jon@dbs.org>
2022-06-01 12:00:33 -0500

    move index.js to src

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Datatable from './lib/Datatable.svelte'
+import SearchInput from './lib/components/SearchInput.svelte'
+import ColumnFilterInputs from './lib/components/ColumnFilterInputs.svelte'
+
+export {
+    Datatable,
+    SearchInput,
+    ColumnFilterInputs
+}
