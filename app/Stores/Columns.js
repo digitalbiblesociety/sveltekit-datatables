f32cce88872819c5828cca9c885f28a46d9b51e7
jonbitgood <jon@dbs.org>
2022-06-22 18:13:39 +0000

    Deploying to package from @ digitalbiblesociety/sveltekit-datatables@be7e8756de4129747ea52a8f76a8649fdea9c278 🚀

--- app/Stores/Columns.js
+++ app/Stores/Columns.js
@@ -1,114 +1,109 @@
-import { writable } from 'svelte/store'
-import { context } from '../context.js'
+import { writable } from 'svelte/store';
+import { context } from '../Context.js';
 
-export default class Columns 
-{
-    create(data, states, filters, options)
-    {
-		const id = states.id
-		this.id = id.get()
-		const pageNumber = states.pageNumber
-		const localFilters = filters.localFilters
-        const { subscribe, set, update } = writable([])
+export default class Columns {
+	create(data, states, filters, options) {
+		const id = states.id;
+		this.id = id.get();
+		const pageNumber = states.pageNumber;
+		const localFilters = filters.localFilters;
+		const { subscribe, set, update } = writable([]);
 		return {
-			subscribe, set, update,
+			subscribe,
+			set,
+			update,
 			get: (self) => {
-				let $columns
-				self.subscribe(store => $columns = store)
-				return $columns
+				let $columns;
+				self.subscribe((store) => ($columns = store));
+				return $columns;
 			},
 			sort: (element, key, context) => {
 				if (options.get(options).sortable !== true || typeof key === 'undefined') {
-					return
+					return;
 				}
 
 				// Grok Direction
-				let sortDir = (element.classList.contains('asc')) ? 'desc' : 'asc'
-				Array.from(element.parentNode.children).forEach((item) => item.classList.remove('asc', 'desc'))
-				element.classList.add(sortDir)
-				
+				let sortDir = element.classList.contains('asc') ? 'desc' : 'asc';
+				Array.from(element.parentNode.children).forEach((item) =>
+					item.classList.remove('asc', 'desc')
+				);
+				element.classList.add(sortDir);
+
 				// Sort
-				data.sort(key, sortDir)
-				pageNumber.set(1, context)
-				this.get(this.id).redraw()
+				data.sort(key, sortDir);
+				pageNumber.set(1, context);
+				this.get(this.id).redraw();
 			},
 			filter: (key, value, operation) => {
-				pageNumber.set(1)
-				localFilters.add(key, value, operation)
-				this.get(this.id).redraw()
+				pageNumber.set(1);
+				localFilters.add(key, value, operation);
+				this.get(this.id).redraw();
 			},
 			draw: () => {
 				setTimeout(() => {
-					const tbody = document.querySelector(`#${id.get()} table tbody tr`)
-					if (tbody === null) return
-					const thead = document.querySelectorAll(`#${id.get()} .dt-header thead tr`)
-					const $columns = this.getData(this.id)
+					const tbody = document.querySelector(`#${id.get()} table tbody tr`);
+					if (tbody === null) return;
+					const thead = document.querySelectorAll(`#${id.get()} .dt-header thead tr`);
+					const $columns = this.getData(this.id);
 
-					const th = thead[0].children[0]
-					const td = Array.from(tbody.children)[0]
+					thead.forEach((tr) => {
+						let i = 0;
+						Array.from(tbody.children).forEach((td) => {
+							let th = tr.children[i];
 
-					thead.forEach(tr => {
-						let i = 0
-						Array.from(tbody.children).forEach(td => {
-							let th = tr.children[i]
-
-							let thW = th.getBoundingClientRect().width
-							let tdW = td.getBoundingClientRect().width
-							if (tdW > thW) { 
-								th.style.minWidth = tdW + 'px'
-								th.style.maxWidth = tdW + 'px'
-								$columns[i].minWidth = tdW
+							let thW = th.getBoundingClientRect().width;
+							let tdW = td.getBoundingClientRect().width;
+							if (tdW > thW) {
+								th.style.minWidth = tdW + 'px';
+								th.style.maxWidth = tdW + 'px';
+								$columns[i].minWidth = tdW;
+							} else {
+								td.style.minWidth = thW + 'px';
+								td.style.maxWidth = thW + 'px';
+								$columns[i].minWidth = thW;
 							}
-							else {
-								td.style.minWidth = thW + 'px'
-								td.style.maxWidth = thW + 'px'
-								$columns[i].minWidth = thW
-							} 
-							i++
-						})
-					})
-				}, 50)	
+							i++;
+						});
+					});
+				}, 50);
 			},
 			redraw: () => {
-				if ( options.get(options).scrollY === false ) return
-				
-				setTimeout(() => {
-					const tbody = document.querySelector(`#${id.get()} table tbody tr`)
-					if (tbody === null) return
-					const thead = document.querySelectorAll(`#${id.get()} .dt-header thead tr`)
-					const $columns = this.getData(this.id)
-					thead.forEach(tr => {
-						let i = 0
-						Array.from(tbody.children).forEach(td => {
-							let th = tr.children[i]
-							let thW = th.getBoundingClientRect().width
-							let tdW = td.getBoundingClientRect().width
-							if (tdW > thW) { 
-								th.style.minWidth = tdW + 'px'
-								th.style.maxWidth = tdW + 'px'
-								$columns[i].minWidth = tdW
-							}
-							else {
-								td.style.minWidth = thW + 'px'
-								td.style.maxWidth = thW + 'px'
-								$columns[i].minWidth = thW
-							} 
-							i++
-						})
-					})
-				}, 50)			
-			},
-		}
-    }
+				if (options.get(options).scrollY === false) return;
 
-	get(id)
-	{
-		return context.get(id).getColumns()
+				setTimeout(() => {
+					const tbody = document.querySelector(`#${id.get()} table tbody tr`);
+					if (tbody === null) return;
+					const thead = document.querySelectorAll(`#${id.get()} .dt-header thead tr`);
+					const $columns = this.getData(this.id);
+					thead.forEach((tr) => {
+						let i = 0;
+						Array.from(tbody.children).forEach((td) => {
+							let th = tr.children[i];
+							let thW = th.getBoundingClientRect().width;
+							let tdW = td.getBoundingClientRect().width;
+							if (tdW > thW) {
+								th.style.minWidth = tdW + 'px';
+								th.style.maxWidth = tdW + 'px';
+								$columns[i].minWidth = tdW;
+							} else {
+								td.style.minWidth = thW + 'px';
+								td.style.maxWidth = thW + 'px';
+								$columns[i].minWidth = thW;
+							}
+							i++;
+						});
+					});
+				}, 50);
+			}
+		};
 	}
 
-	getData(id) 
-	{
-		const columns =  context.get(id).getColumns()
-		return context.get(id).getColumns().get(columns)
+	get(id) {
+		return context.get(id).getColumns();
+	}
+
+	getData(id) {
+		const columns = context.get(id).getColumns();
+		return context.get(id).getColumns().get(columns);
 	}
 }
--- components/SearchInput.svelte
+++ components/SearchInput.svelte
@@ -1,23 +1,23 @@
 <script>
-	import { context as store } from '../app/context.js'
-    export let id = 'svelte-simple-datatable'
-	export let ref = ''
-	export let classList = ''
+	import { context as store } from '../app/Context.js';
+	export let id = 'svelte-simple-datatable';
+	export let ref = '';
+	export let classList = '';
 
-    const context = store.get(id)
-	const options = context.getOptions()
+	const context = store.get(id);
+	const options = context.getOptions();
 
 	const search = (value) => {
-		context.getPageNumber().set(1)
-		context.getGlobalFilter().set(value)
-		context.getColumns().redraw()
-	}
+		context.getPageNumber().set(1);
+		context.getGlobalFilter().set(value);
+		context.getColumns().redraw();
+	};
 </script>
 
 <input
-		class={classList}
-		type="text"
-   		placeholder={$options?.labels?.search ?? 'Search'}
-		on:input={(e) => search(e.target.value)}
-		{ref}
-	/>
+	class={classList}
+	type="text"
+	placeholder={$options?.labels?.search ?? 'Search'}
+	on:input={(e) => search(e.target.value)}
+	{ref}
+/>
